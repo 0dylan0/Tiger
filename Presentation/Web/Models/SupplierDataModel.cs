545753289889b025dcead4c3ffd6a4bb7a947e86
LQT <LQT@LIQINGTAO>
2016-12-20 22:37:39 +0800

    供货商功能初步完成

--- Presentation/Web/Models/SupplierDataModel.cs
+++ Presentation/Web/Models/SupplierDataModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,62 +10,61 @@ namespace Web.Models
     {
         public int ID { get; set; }
 
-        //供应商名称
+        [DisplayName("供应商名称")]
         public string SupplierName { get; set; }
 
-        //公司名称
+        [DisplayName("公司名称")]
         public string CompanyName { get; set; }
 
-        //联系人
+        [DisplayName("联系人")]
         public string Contacts { get; set; }
 
-        //供应商类别
+        [DisplayName("供应商类别")]
         public string SupplierType { get; set; }
 
-        //区域
+        [DisplayName("区域")]
         public string Area { get; set; }
 
-        //地址
+        [DisplayName("地址")]
         public string Address { get; set; }
 
-        //手机
+        [DisplayName("手机")]
         public string Phone { get; set; }
 
-        //座机
+        [DisplayName("座机")]
         public string Telephone { get; set; }
 
-
-        //欠款
+        [DisplayName("欠款")]
         public string Arrears { get; set; }
 
-        //还款日期
+        [DisplayName("还款日期")]
         public DateTime RepaymentDate { get; set; }
 
-        //开户行
+        [DisplayName("开户行")]
         public string Banks { get; set; }
 
-        //开户名
+        [DisplayName("开户名")]
         public string AccountName { get; set; }
 
-        //银行账号
+        [DisplayName("银行账号")]
         public string BankAccount { get; set; }
 
-        //税号
+        [DisplayName("税号")]
         public string TaxIdentificationNumber { get; set; }
 
-        //排序号
+        [DisplayName("排序号")]
         public string Seq { get; set; }
 
-        //备注1
+        [DisplayName("备注1")]
         public string Remarks1 { get; set; }
 
-        //备注2
+        [DisplayName("备注2")]
         public string Remarks2 { get; set; }
 
-        //备注3
+        [DisplayName("备注3")]
         public string Remarks3 { get; set; }
 
-        //备注4
+        [DisplayName("备注4")]
         public string Remarks4 { get; set; }
     }
 }
