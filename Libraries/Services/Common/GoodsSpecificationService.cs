479edc57f9865ee1b4d15cf3040f24888c10be41
LQT <LQT@LIQINGTAO>
2017-01-04 22:34:48 +0800

    商品规格 功能完成

--- Libraries/Services/Common/GoodsSpecificationService.cs
+++ Libraries/Services/Common/GoodsSpecificationService.cs
@@ -1,4 +1,7 @@
-﻿using Data;
+﻿using Core.Domain.Common;
+using Core.Page;
+using Dapper;
+using Data;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -18,6 +21,52 @@ namespace Services.Common
         {
             _repository = repository;
             _context = context;
+        }
+
+        public void Insert(GoodsSpecification GoodsSpecification)
+        {
+            var sql = $@"insert into GoodsSpecification(
+                    Name)
+			        VALUES (
+                    @Name)";
+            _context.Execute(sql, new
+            {
+                Name = GoodsSpecification.Name
+            });
+        }
+
+        public void Update(GoodsSpecification GoodsSpecification)
+        {
+            var sql = $@"update GoodsSpecification set
+                    Name=@Name
+                    where ID=@ID";
+            _context.Execute(sql, new
+            {
+                ID = GoodsSpecification.ID,
+                Name = GoodsSpecification.Name
+            });
+        }
+
+        public GoodsSpecification GetById(int id)
+        {
+            var sql = @"select * from GoodsSpecification  where id = @id";
+            return _context.QuerySingle<GoodsSpecification>(sql, new
+            {
+                id = id
+            });
+        }
+        public IPagedList<GoodsSpecification> GetList(string textQuery, int pageIndex = 0, int pageSize = 2147483647, string sortExpression = "")
+        {
+            string sql = @"select * from GoodsSpecification";
+            var Parameter = new DynamicParameters();
+            return new SqlPagedList<GoodsSpecification>(sql, Parameter, pageIndex, pageSize, sortExpression);
+        }
+
+        public List<GoodsSpecification> GetGoodsSpecificationList()
+        {
+            string sql = @"select id as code, name from GoodsSpecification";
+
+            return _context.Query<GoodsSpecification>(sql).ToList();
         }
     }
 }
--- Presentation/Web/Controllers/GoodsSpecificationController.cs
+++ Presentation/Web/Controllers/GoodsSpecificationController.cs
@@ -1,4 +1,6 @@
 ﻿using Core;
+using Core.Domain.Common;
+using Core.Page;
 using Services.Common;
 using Services.Localization;
 using System;
@@ -6,7 +8,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Extensions;
 using Web.Framework.Controllers;
+using Web.Framework.Json;
+using Web.Framework.Page;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -14,21 +20,78 @@ namespace Web.Controllers
     {
 
         private readonly IWorkContext _webWorkContext;
-        private readonly GoodsDataService _goodsDataService;
+        private readonly GoodsSpecificationService _goodsSpecificationService;
         private readonly LocalizationService _localizationService;
         public GoodsSpecificationController(IWorkContext webWorkContext,
-            GoodsDataService goodsDataService,
+            GoodsSpecificationService goodsSpecificationService,
             LocalizationService localizationService)
         {
             _webWorkContext = webWorkContext;
-            _goodsDataService = goodsDataService;
+            _goodsSpecificationService = goodsSpecificationService;
             _localizationService = localizationService;
         }
 
-        // GET: GoodsSpecification
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            GoodsSpecificationListModel model = new GoodsSpecificationListModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PageInfo pageInfo, GoodsSpecificationListModel model)
+        {
+            IPagedList<GoodsSpecification> GoodsSpecificationList = _goodsSpecificationService.GetList(model.Name, pageInfo.PageIndex, pageInfo.PageSize, pageInfo.sortExpression);
+            model.GoodsSpecification = GoodsSpecificationList.MapTo<IList<GoodsSpecification>, IList<GoodsSpecificationModel>>();
+
+            var results = new DataTable<GoodsSpecificationModel>()
+            {
+                Draw = pageInfo.Draw + 1,
+                RecordsTotal = GoodsSpecificationList.TotalCount,
+                RecordsFiltered = GoodsSpecificationList.TotalCount,
+                Data = model.GoodsSpecification
+            };
+            return Json(new PlainJsonResponse(results));
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            GoodsSpecificationModel model = new GoodsSpecificationModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(GoodsSpecificationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                GoodsSpecification GoodsSpecification = model.MapTo<GoodsSpecificationModel, GoodsSpecification>();
+                _goodsSpecificationService.Insert(GoodsSpecification);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var goodsSpecification = _goodsSpecificationService.GetById(id);
+            var res = goodsSpecification.MapTo<GoodsSpecification, GoodsSpecificationModel>();
+            return View(res);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(GoodsSpecificationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                GoodsSpecification goodsSpecification = model.MapTo<GoodsSpecificationModel, GoodsSpecification>();
+                _goodsSpecificationService.Update(goodsSpecification);
+                SuccessNotification($"{_localizationService.GetResource("UpdateSuccess") }");
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
         }
     }
 }
