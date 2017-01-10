1eab4bede5e6c18c7ff303b7574454462ed45bb6
LQT <LQT@LIQINGTAO>
2017-01-10 16:49:15 +0800

    仓库验证完成

--- Presentation/Web/Models/WarehouseModel.cs
+++ Presentation/Web/Models/WarehouseModel.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using FluentValidation.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
+using Web.Validators;
 
 namespace Web.Models
 {
+    [Validator(typeof(WarehouseValidator))]
     public class WarehouseModel
     {
         public int ID { get; set; }
 
+        [DisplayName("仓库名")]
         public string Name { get; set; }
     }
 }
