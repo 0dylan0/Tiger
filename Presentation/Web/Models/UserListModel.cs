3e4af651f3d43d43c8a493ceaba8e500497fd2b2
LQT <LQT@LIQINGTAO>
2017-01-11 13:46:24 +0800

    修改 用户 管理样式

--- Presentation/Web/Models/UserListModel.cs
+++ Presentation/Web/Models/UserListModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Web.Models
 {
     public class UserListModel
     {
+        [DisplayName("用户名")]
         public string Name { get; set; }
 
         public IList<UserModel> UserData { get; set; } 
--- Presentation/Web/Models/UserModel.cs
+++ Presentation/Web/Models/UserModel.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using Web.Validators;
@@ -12,8 +13,10 @@ namespace Web.Models
     {
         public int ID { get; set; }
 
+        [DisplayName("用户名")]
         public string Name { get; set; }
 
+        [DisplayName("密码")]
         public string Password { get; set; }
     }
 }
